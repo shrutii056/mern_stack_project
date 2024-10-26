577e38ca18bb32cbdd1c175e1f7c872f0744430f
shrutii056 <139227609+shrutii056@users.noreply.github.com>
2024-10-26 12:50:41 +0530

    Update Create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -11,7 +11,7 @@ const Create = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const addUser = { name, email, age };
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("http://localhost:5000/api/users", {
       method: "POST",
       body: JSON.stringify(addUser),
       headers: {
